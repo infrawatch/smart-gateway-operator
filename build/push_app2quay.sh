9521cfa68494d3dd3256b10ebe0018e9bbb51e69
Leif Madsen <lmadsen@redhat.com>
2020-01-13 18:22:03 -0500

    Add more parameters to push_app2quay.sh

--- build/push_app2quay.sh
+++ build/push_app2quay.sh
@@ -3,18 +3,27 @@
 
 set -e
 
-CSV_VERSION=${1:-0.2.0}
+CSV_VERSION=${CSV_VERSION:-0.2.0}
 UNIXDATE=$(date +%s)
+ORGANIZATION=${ORGANIZATION:-redhat-service-assurance}
 
-echo -n "Username: "
-read USERNAME
-echo -n "Password: "
-read -s PASSWORD
+if [ -z "$USERNAME" ]; then
+    echo -n "Username: "
+    read USERNAME
+fi
+
+if [ -z "$PASSWORD" ]; then
+    echo -n "Password: "
+    read -s PASSWORD
+fi
 echo
 
 AUTH_TOKEN=$(curl -sH "Content-Type: application/json" \
 -XPOST https://quay.io/cnr/api/v1/users/login \
 -d '{"user": {"username": "'"${USERNAME}"'", "password": "'"${PASSWORD}"'"}}' | jq -r '.token')
 
-operator-courier push "./deploy/olm-catalog/smart-gateway-operator" "redhat-service-assurance" "smartgateway-operator" "${CSV_VERSION}-${UNIXDATE}" "${AUTH_TOKEN}"
+# The application registry name and the container registry name can not be the same on quay.io. Same as we do with the smart-gateway-operator we remove the first
+# instance of the hyphen for the application registry (serviceassurance-operator). For the container registry we match the git repository name (service-assurance-operator).
+operator-courier push "./deploy/olm-catalog/smart-gateway-operator" "${ORGANIZATION}" "smartgateway-operator" "${CSV_VERSION}-${UNIXDATE}" "${AUTH_TOKEN}"
+
 # vim: set ft=bash:
