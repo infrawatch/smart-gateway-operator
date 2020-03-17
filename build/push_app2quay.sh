44f3991399b8dcdd2e197b16b34559f6e2abbf43
Leif Madsen <lmadsen@redhat.com>
2020-03-17 15:59:57 -0400

    Stop using unixdate on the end of the application registry version

--- build/push_app2quay.sh
+++ build/push_app2quay.sh
@@ -21,6 +21,6 @@ AUTH_TOKEN=$(curl -sH "Content-Type: application/json" \
 
 # The application registry name and the container registry name can not be the same on quay.io. Same as we do with the smart-gateway-operator we remove the first
 # instance of the hyphen for the application registry (servicetelemetry-operator). For the container registry we match the git repository name (service-telemetry-operator).
-operator-courier push "./deploy/olm-catalog/smart-gateway-operator" "${ORGANIZATION}" "smartgateway-operator" "${CSV_VERSION}-${UNIXDATE}" "${AUTH_TOKEN}"
+operator-courier push "./deploy/olm-catalog/smart-gateway-operator" "${ORGANIZATION}" "smartgateway-operator" "${CSV_VERSION}" "${AUTH_TOKEN}"
 
 # vim: set ft=bash:
