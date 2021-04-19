53b737476eefefaf21cfff8f7807e216b56c0338
Leif Madsen <lmadsen@redhat.com>
2021-04-19 08:37:33 -0400

    Make generating CSVs possible via templated CSV (#82)
    * Make generating CSVs possible via templated CSV
    
    Adjust the deploy/ directory contents so that generating a CSV is
    possible for release automation. Makes use of a UNIX timestamp to
    provide the final version position after defining the major release
    version of the CSV.
    
    * Update CI
    
    * Resolve issues with CI runs

--- build/update_csv.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-set -e
-REL=$(dirname "$0"); source "${REL}/metadata.sh"
-operator-sdk generate csv --csv-version=${CSV_VERSION} --operator-name smart-gateway-operator --update-crds
