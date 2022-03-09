a400312a015388808f8c51dcae0dbfd3cc5d1d68
Leif Madsen <lmadsen@redhat.com>
2022-03-09 09:38:50 -0500

    Revert "Migrate to operator-sdk 1.16.0 (#108)"
    Revert changes to migrate Smart Gateway Operator to operator-sdk 1.16.0
    until the same set of changes can be made to Service Telemetry Operator.
    
    Changing how the operator is built causes a regression in the testing
    framework for STF. Instead of making temporary changes to the testing
    framework to deal with the changes in Operator-SDK 1.16.0, we'll make
    changes to the testing framework when migrating Service Telemetry
    Operator to operator-sdk 1.16.0 as well.
    
    In the meantime, revert these changes which will just end up landing in
    conjunction with the changes to Service Telemetry Operator as a bundled
    change set.
    
    This reverts commit 8328ed835db1afd9d33120cc19badf7773f4d86c.

--- build/generate_bundle.sh
+++ build/generate_bundle.sh
@@ -12,29 +12,44 @@ generate_version() {
     echo "---- Operator Version: ${OPERATOR_BUNDLE_VERSION}"
 }
 
+create_working_dir() {
+    echo "-- Create working directory"
+    WORKING_DIR=${WORKING_DIR:-"/tmp/${OPERATOR_NAME}-bundle-${OPERATOR_BUNDLE_VERSION}"}
+    mkdir -p "${WORKING_DIR}"
+    echo "---- Created working directory: ${WORKING_DIR}"
+}
+
+generate_dockerfile() {
+    echo "-- Generate Dockerfile for bundle"
+    sed -E "s#<<OPERATOR_BUNDLE_VERSION>>#${OPERATOR_BUNDLE_VERSION}#g;s#<<BUNDLE_CHANNELS>>#${BUNDLE_CHANNELS}#g;s#<<BUNDLE_DEFAULT_CHANNEL>>#${BUNDLE_DEFAULT_CHANNEL}#g" "${REL}/../${BUNDLE_PATH}/Dockerfile.in" > "${WORKING_DIR}/Dockerfile"
+    echo "---- Generated Dockerfile complete"
+}
+
 generate_bundle() {
     echo "-- Generate bundle"
     REPLACE_REGEX="s#<<CREATED_DATE>>#${CREATED_DATE}#g;s#<<OPERATOR_IMAGE>>#${OPERATOR_IMAGE}#g;s#<<OPERATOR_TAG>>#${OPERATOR_TAG}#g;s#<<RELATED_IMAGE_BRIDGE_SMARTGATEWAY>>#${RELATED_IMAGE_BRIDGE_SMARTGATEWAY}#g;s#<<RELATED_IMAGE_BRIDGE_SMARTGATEWAY_TAG>>#${RELATED_IMAGE_BRIDGE_SMARTGATEWAY_TAG}#g;s#<<RELATED_IMAGE_CORE_SMARTGATEWAY>>#${RELATED_IMAGE_CORE_SMARTGATEWAY}#g;s#<<RELATED_IMAGE_CORE_SMARTGATEWAY_TAG>>#${RELATED_IMAGE_CORE_SMARTGATEWAY_TAG}#g;s#<<OPERATOR_BUNDLE_VERSION>>#${OPERATOR_BUNDLE_VERSION}#g;s#1.99.0#${OPERATOR_BUNDLE_VERSION}#g;s#<<BUNDLE_OLM_SKIP_RANGE_LOWER_BOUND>>#${BUNDLE_OLM_SKIP_RANGE_LOWER_BOUND}#g"
 
     pushd "${REL}/../"
-    make bundle CHANNELS=${BUNDLE_CHANNELS} DEFAULT_CHANNEL=${BUNDLE_DEFAULT_CHANNEL} VERSION="${OPERATOR_BUNDLE_VERSION}" #--output-dir "${WORKING_DIR}"
+    ${OPERATOR_SDK} generate bundle --channels ${BUNDLE_CHANNELS} --default-channel ${BUNDLE_DEFAULT_CHANNEL} --manifests --metadata --version "${OPERATOR_BUNDLE_VERSION}" --output-dir "${WORKING_DIR}"
     popd
 
     echo "---- Replacing variables in generated manifest"
-    sed -i -E "${REPLACE_REGEX}" "bundle/manifests/${OPERATOR_NAME}.clusterserviceversion.yaml"
-    echo "---- Generated bundle complete at bundle/manifests/${OPERATOR_NAME}.clusterserviceversion.yaml"
+    sed -i -E "${REPLACE_REGEX}" "${WORKING_DIR}/manifests/${OPERATOR_NAME}.clusterserviceversion.yaml"
+    echo "---- Generated bundle complete at ${WORKING_DIR}/manifests/${OPERATOR_NAME}.clusterserviceversion.yaml"
 }
 
 build_bundle_instructions() {
-    echo "***"
-    echo "Bundle has been created locally. To build and push the bundle to your remote registry run:"
-    echo "> make bundle-build bundle-push"
-    echo "***"
+    echo "-- Commands to create a bundle build"
+    echo docker build -t "${OPERATOR_BUNDLE_IMAGE}:${OPERATOR_BUNDLE_VERSION}" -f "${WORKING_DIR}/Dockerfile" "${WORKING_DIR}"
+    echo docker push ${OPERATOR_BUNDLE_IMAGE}:${OPERATOR_BUNDLE_VERSION}
 }
+
 
 # generate templates
 echo "## Begin bundle creation"
 generate_version
+create_working_dir
+generate_dockerfile
 generate_bundle
 build_bundle_instructions
 echo "## End Bundle creation"
