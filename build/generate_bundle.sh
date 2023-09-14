3b39aa8ce7009709d2259d47cb465795e61f8d18
Emma Foley <efoley@redhat.com>
2023-09-14 15:29:39 +0100

    generate_bundle: truncate logfile, don't log pushd/popd

--- build/generate_bundle.sh
+++ build/generate_bundle.sh
@@ -2,6 +2,8 @@
 set -e
 set -x
 LOGFILE=${LOGFILE:-/dev/null}
+truncate --size=0 $LOGFILE
+
 OPERATOR_SDK=${OPERATOR_SDK:-operator-sdk}
 REL=$( readlink -f $(dirname "$0"))
 
@@ -25,15 +27,15 @@ generate_dockerfile() {
 generate_bundle() {
     REPLACE_REGEX="s#<<CREATED_DATE>>#${CREATED_DATE}#g;s#<<OPERATOR_IMAGE>>#${OPERATOR_IMAGE}#g;s#<<OPERATOR_TAG>>#${OPERATOR_TAG}#g;s#<<RELATED_IMAGE_BRIDGE_SMARTGATEWAY>>#${RELATED_IMAGE_BRIDGE_SMARTGATEWAY}#g;s#<<RELATED_IMAGE_BRIDGE_SMARTGATEWAY_TAG>>#${RELATED_IMAGE_BRIDGE_SMARTGATEWAY_TAG}#g;s#<<RELATED_IMAGE_CORE_SMARTGATEWAY>>#${RELATED_IMAGE_CORE_SMARTGATEWAY}#g;s#<<RELATED_IMAGE_CORE_SMARTGATEWAY_TAG>>#${RELATED_IMAGE_CORE_SMARTGATEWAY_TAG}#g;s#<<RELATED_IMAGE_OAUTH_PROXY>>#${RELATED_IMAGE_OAUTH_PROXY}#g;s#<<RELATED_IMAGE_OAUTH_PROXY_TAG>>#${RELATED_IMAGE_OAUTH_PROXY_TAG}#g;s#<<OPERATOR_BUNDLE_VERSION>>#${OPERATOR_BUNDLE_VERSION}#g;s#1.99.0#${OPERATOR_BUNDLE_VERSION}#g;s#<<BUNDLE_OLM_SKIP_RANGE_LOWER_BOUND>>#${BUNDLE_OLM_SKIP_RANGE_LOWER_BOUND}#g"
 
-    pushd "${REL}/../" >> ${LOGFILE} 2>&1
+    pushd "${REL}/../" > /dev/null 2>&1
     ${OPERATOR_SDK} generate bundle --channels ${BUNDLE_CHANNELS} --default-channel ${BUNDLE_DEFAULT_CHANNEL} --manifests --metadata --version "${OPERATOR_BUNDLE_VERSION}" --output-dir "${WORKING_DIR}" >> ${LOGFILE} 2>&1
-    popd >> ${LOGFILE} 2>&1
+    popd > /dev/null 2>&1
 
     sed -i -E "${REPLACE_REGEX}" "${WORKING_DIR}/manifests/${OPERATOR_NAME}.clusterserviceversion.yaml"
 }
 
 copy_extra_metadata() {
-    pushd "${REL}/../" >> ${LOGFILE} 2>&1
+    pushd "${REL}/../" > /dev/null 2>&1
     cp -r ./deploy/olm-catalog/smart-gateway-operator/tests/ "${WORKING_DIR}"
     cp ./deploy/olm-catalog/smart-gateway-operator/metadata/properties.yaml "${WORKING_DIR}/metadata/"
 }
@@ -45,7 +47,6 @@ build_bundle_instructions() {
 }
 
 
-${OPERATOR_SDK} version
 # generate templates
 generate_version
 create_working_dir
@@ -53,6 +54,8 @@ generate_dockerfile
 generate_bundle
 copy_extra_metadata
 #build_bundle_instructions
+
+set +x
 
 JSON_OUTPUT='{"operator_bundle_image":"%s","operator_bundle_version":"%s","operator_image":"%s","bundle_channels":"%s","bundle_default_channel":"%s","operator_tag":"%s","working_dir":"%s"}'
 printf "$JSON_OUTPUT" "$OPERATOR_BUNDLE_IMAGE" "$OPERATOR_BUNDLE_VERSION" "$OPERATOR_IMAGE" "$BUNDLE_CHANNELS" "$BUNDLE_DEFAULT_CHANNEL" "$OPERATOR_TAG" "$WORKING_DIR"
